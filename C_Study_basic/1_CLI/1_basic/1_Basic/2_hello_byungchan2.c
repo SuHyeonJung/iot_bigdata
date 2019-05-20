be41ed22c8ec8b126c62eff60a1a194f07bc7a67
MSDN-SPECIAL <galma94815@naver.com>
2019-05-20 09:45:22 +0900

    최신 업데이트

--- C_Study_basic/1_CLI/1_basic/1_Basic/2_hello_byungchan2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main (){
-printf("Hello ByungChan!\n");
-printf("Hello World!");
-}
--- C_Study_basic/1_CLI/1_basic/99_연습문제/99_my_test/1_test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void main(){
-	printf("È«\n");
-	printf("È«±æ\n");
-	printf("È«±æµ¿\n");
-}
